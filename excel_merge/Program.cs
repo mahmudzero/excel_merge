4edbdb43d80561442ac90dea1ca7e08c0f3787a5
Mahmud Ahmad <AhmadM@vitecnet.local>
2017-06-16 10:12:35 -0400

    initial commit

--- excel_merge/Program.cs
+++ excel_merge/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace excel_merge
 {
     class Program
     {
+
+        public void CreatDoc(String fileName, SpreadsheetDocumentType documentType)
+        {
+            using (SpreadsheetDocument excelSpreadsheet = SpreadsheetDocument.Create(fileName, documentType))
+            {
+                WorkbookPart workbookPart = excelSpreadsheet.AddWorkbookPart();
+            }
+        }
+
         static void Main(string[] args)
         {
+            Debug.WriteLine("Running");
         }
     }
 }
